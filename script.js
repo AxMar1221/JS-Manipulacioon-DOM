9b453c6e8fdbc50b00129efc6cf7c6b5081ba687
Tachi Dito <tachidito_7inc@hotmail.com>
2023-06-30 23:08:50 -0600

    fixed data-task HTML/ const content & taks inneHTML Script

--- script.js
+++ script.js
@@ -9,7 +9,15 @@ const createTask = (e) => {
   const input = document.querySelector("[data-form-input]");
   const value = input.value;
   input.value = "";
+  const task = document.querySelector("[data-task]");
+  const content = `<div>
+    <i class="far fa-check-square icon"></i>
+    <span class="task">${value}</span>
+    </div>
+    <i class="fas fa-trash-alt trashIcon icon"></i>`;
+  task.innerHTML = content;
   console.log(value);
+  console.log(content);
 };
 
 console.log(btn);
