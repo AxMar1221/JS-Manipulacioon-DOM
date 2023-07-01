d3882f5c7b36104c79265ddd0b95807a376a2b3f
Tachi Dito <tachidito_7inc@hotmail.com>
2023-06-30 23:40:06 -0600

    create elements-HTML/ add values to elements-HTML

--- script.js
+++ script.js
@@ -8,23 +8,37 @@ const createTask = (e) => {
   e.preventDefault();
   const input = document.querySelector("[data-form-input]");
   const value = input.value;
-  const list = document.querySelector("[data-list]")
-  input.value = "";
+  const list = document.querySelector("[data-list]");
   const task = document.createElement("li");
-  task.classList.add('card')
-  const content = `<div>
-    <i class="far fa-check-square icon"></i>
-    <span class="task">${value}</span>
-    </div>
+  task.classList.add("card");
+  input.value = "";
+
+  const taskContent = document.createElement('div');
+  taskContent.appendChild(checkComplete());
+  
+  const titleTask = document.createElement('span');
+  titleTask.classList.add('task');
+  titleTask.innerText = value;
+  taskContent.appendChild(titleTask);
+
+  const content = `
     <i class="fas fa-trash-alt trashIcon icon"></i>`;
 
-  task.innerHTML = content;
+//   task.innerHTML = content;
+  task.appendChild(taskContent)
 
-  list.appendChild(task)
+  list.appendChild(task);
 
   console.log(value);
 };
 
-console.log(btn);
-
 btn.addEventListener("click", createTask);
+
+const checkComplete = () => {
+  const i = document.createElement("i");
+  i.classList.add("far");
+  i.classList.add("fa-check-square");
+  i.classList.add("icon");
+
+  return i;
+};
