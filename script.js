f5663564d203e1892720a9028630e09b2b995f34
Tachi Dito <tachidito_7inc@hotmail.com>
2023-06-30 23:54:37 -0600

    create & update function-completeTask

--- script.js
+++ script.js
@@ -35,6 +35,14 @@ btn.addEventListener("click", createTask);
 const checkComplete = () => {
   const i = document.createElement("i");
   i.classList.add("far", "fa-check-square", "icon");
+  i.addEventListener('click', completeTask);
 
   return i;
 };
+
+const completeTask = (event) => {
+    const ele = event.target
+    ele.classList.toggle('fas')
+    ele.classList.toggle('completeIcon')
+    ele.classList.toggle('far')
+}
