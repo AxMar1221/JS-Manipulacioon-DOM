1ae4b991ab8b8b258aa2d8e6d3dae72f7706dd86
Tachi Dito <tachidito_7inc@hotmail.com>
2023-06-30 21:49:13 -0600

    optimized input function

--- script.js
+++ script.js
@@ -4,11 +4,12 @@ window.addEventListener("load", () => {
 
 const btn = document.querySelector("[data-form-btn]");
 
+const createTask = (e) => {
+  e.preventDefault();
+  const input = document.querySelector("[data-form-input]");
+  console.log(input.value);
+};
+
 console.log(btn);
 
-btn.addEventListener('click', (e) => {
-    e.preventDefault(btn)
-    const input = document.querySelector("[data-form-input]");
-    console.log(input.value);
-})
-
+btn.addEventListener("click", createTask);
