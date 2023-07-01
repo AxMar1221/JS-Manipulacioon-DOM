5f751098e12e05db3f499b5a3654fa5209de6d97
Tachi Dito <tachidito_7inc@hotmail.com>
2023-06-30 23:16:36 -0600

    fixed data-list HTML/ const list & create element/add-class Script

--- script.js
+++ script.js
@@ -8,16 +8,21 @@ const createTask = (e) => {
   e.preventDefault();
   const input = document.querySelector("[data-form-input]");
   const value = input.value;
+  const list = document.querySelector("[data-list]")
   input.value = "";
-  const task = document.querySelector("[data-task]");
+  const task = document.createElement("li");
+  task.classList.add('card')
   const content = `<div>
     <i class="far fa-check-square icon"></i>
     <span class="task">${value}</span>
     </div>
     <i class="fas fa-trash-alt trashIcon icon"></i>`;
+
   task.innerHTML = content;
+
+  list.appendChild(task)
+
   console.log(value);
-  console.log(content);
 };
 
 console.log(btn);
